ed30acecd16fa25d3c5fc78320fec763a8b31ea1
2381024 <2381024@unai.edu>
2024-11-17 00:44:44 +0700

    Database Implementation Handson 9

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import config.Database;
 import repositories.TodoListRepository;
 import repositories.TodoListRepositoryImpl;
 import services.TodoListServiceImpl;
@@ -7,6 +8,9 @@ import views.TodoListView;
 
 public class Main {
     public static void main(String[] args) {
+        Database database = new Database("todo", "root", "", "localhost","3306");
+        database.setup();
+
         TodoListRepository todoListRepository = new TodoListRepositoryImpl();
         TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
         TodoListView todoListView = new TodoListTerminalViewImpl(todoListService);
